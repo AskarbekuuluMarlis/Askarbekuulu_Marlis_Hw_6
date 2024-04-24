0041d34273d00077c882246b99d64d460b7636f8
AskarbekuuluMarlis <marlisaskarbekov00000@gmail.com>
2024-04-24 18:29:45 +0600

    firth commit

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        Boss boss = new Boss("invisible ", "Kinetic ", 54,
-                500, new Weapon("Laser_tech ", WeaponType.LASER_WEAPON));
+        Boss boss = new Boss();
+        boss.setHealth(500);
+        boss.setDamage(50);
+        boss.setDeffense("Kinetic ");
+        boss.setAbility("invisible");
+        boss.setWeapon(new Weapon("Laser-tech ",WeaponType.LASER_WEAPON));
+        System.out.println("Boss info: " + boss.printInfo());
 
         Skeleton skeleton1 = new Skeleton("Fire ", "Magic ", 20,
                 100, new Weapon("Nuclear ", WeaponType.NUCLEAR_WEAPON), 5);
